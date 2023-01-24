b15405619d2f3a8289dd06165ff48fd8d0d4e141
Austin Brown <modparadigm@gmail.com>
2023-01-24 18:24:34 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import setup, find_packages
 
 setup(
   name="mhlb", 
-  version="0.1",
+  version="0.15",
   author="Austin Brown",
   author_email="austin.d.brown@warwick.ac.uk",
-  description="Estimate Lower bounds for Metropolis-Hastings",
+  description="A Python implementation to estimate lower bounds on the geometric convergence rate for RWM Metropolis-Hastings from the pre-print arxiv.org/abs/2212.05955.",
   url = "https://github.com/austindavidbrown/lower-bounds-for-Metropolis-Hastings",
   packages=["mhlb"],
   install_requires=["torch >= 1.9.1"],
